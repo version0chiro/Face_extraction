99ec93a61fa2fc15a37abedf05ecbf951de3d5d3
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2020-09-16 16:25:29 +0530

    added code for cheek extraction and removed .dat files

--- face_extractor.py
+++ face_extractor.py
@@ -5,3 +5,38 @@ import glob
 from skimage import io
 import numpy as np
 import cv2
+from imutils import face_utils
+
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (1280, 720))
+
+predictor_path = 'shape_predictor_68_face_landmarks.dat'
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    dets = detector(frame, 0)
+    for k, d in enumerate(dets):
+        shape = predictor(frame, d)
+        shape = face_utils.shape_to_np(shape)
+        # landmarks = np.matrix([[p.x, p.y] for p in shape.parts()])
+        # for num in range(shape.num_parts):
+            # cv2.circle(frame, (shape.parts()[num].x, shape.parts()[num].y), 3, (0,255,0), -1)
+        cv2.rectangle(frame,(shape[29][1],shape[33][1]), (shape[54][0],shape[12][0]),(255,0,0),2)
+            # cv2.rectangle(frame,shape[29][1]:shape[33][1], shape[4][0]:shape[48][0],(255,0,0),2)
+    cv2.imshow('frame', frame)
+    out.write(frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("q pressed")
+        break
+
+
+cap.release()
+out.release()
+
+cv2.destroyAllWindows()
